504231fdca36afdf3d6a35433d79fb0dd64b1ca3
Cuza <baopham92@yahoo.com>
2018-12-19 15:57:35 -0500

    On website load / refresh click action is true.

--- content.js
+++ content.js
@@ -1,1 +1,17 @@
-console.log('content.js script running.');
+window.onload = () => {
+    clickRoll();
+}
+
+const clickRoll = () => {
+    if (freeRoll && freeRoll.style.display === 'none') {
+        return console.log(true, 'btn is not displaying.');
+    } else {
+        return console.log(false, 'btn is displaying.');
+    }
+}
+
+// const timer = document.querySelector('#time_remaining');
+// console.log(timer);
+
+const freeRoll = document.getElementById('free_play_form_button');
+console.log(freeRoll);
