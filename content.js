ba28e1f2d92b18a31434164232239993e12cbf80
Cuza <baopham92@yahoo.com>
2018-12-20 17:15:19 -0500

    Incompleted localStorage logging tracker now functional.

--- content.js
+++ content.js
@@ -8,44 +8,46 @@ const helper = Object.freeze({
 
     variables: {
         elementType: '',
-        counter: 1
+        counter: 0
     },
 
     loggedClicks: () => {
-        if (clickRoll() && cancelModal()) {
-            localStorage.length !== helper.variables.counter ? helper.variables.counter = localStorage.length + 1 : console.log('error updating counter.');
 
-            localStorage.setItem('tInfo' + helper.variables.counter, 'Number of clicks recorded: ' + helper.variables.counter + ' on ' + Date(Date.now()).toString())
+        const logClicks = () => {
+            const cMessage = localStorage.setItem('tInfo' + helper.variables.counter, 'Number of clicks recorded: ' + helper.variables.counter + ' on ' + Date(Date.now()).toString());
+            return cMessage;
+        };
 
-            for (var i = 0; i < localStorage.length; i++) {
-                console.log(localStorage.getItem(localStorage.key(i)));
-            }
-        } 
+        if (localStorage) {
+                helper.variables.counter <= localStorage.length ? helper.variables.counter + localStorage.length + 1 && logClicks() : console.log('Waiting for more rolls!');
+        }
+
+        for (var i = 0; i < localStorage.length; i++) {
+            console.log(localStorage.getItem(localStorage.key(i)));
+        }
     },
 
     cLog: (element) => {
 
         switch (element) {
             case timer: helper.variables.elementType = 'Timer';
-            break;
+                break;
             case freeRoll: helper.variables.elementType = 'Free Roll';
-            break;
+                break;
             case modal: helper.variables.elementType = 'Modal';
-            break;
+                break;
         }
 
         element ? console.log(helper.variables.elementType, 'detected.') : console.log('error detected', helper.variables.elementType, element);
-
-        helper.loggedClicks();
     }
 })
 
 const clickRoll = () => {
     if (freeRoll && freeRoll.style.display === 'none') {
-        return console.log('Unsuccessful');
+        return console.log('Click for rolls currently unavailable.');
     } else {
         freeRoll.click();
-        return console.log('Successful!');
+        return console.log('Roll clicked!');
     }
 }
 
