813bd712dd43136f8857fa622ea01fe7e4c9f34e
Cuza <baopham92@yahoo.com>
2018-12-19 19:09:57 -0500

    Helper methods and modal detection added.

--- content.js
+++ content.js
@@ -3,12 +3,28 @@ window.onload = () => {
     cancelModal();
 }
 
+const helper = Object.freeze({
+    cLog: (element) => {
+
+        switch (element) {
+            case element === timer: element === 'Timer';
+            break;
+            case element === freeRoll: element === 'Free Roll';
+            break;
+            case element === modal: element === 'Modal';
+            break;
+        }
+
+        element ? console.log(`${element} detected.`) : console.log(`error detecting ${element}`);
+    }
+})
+
 const clickRoll = () => {
     if (freeRoll && freeRoll.style.display === 'none') {
-        return console.log(true, 'Unsuccessful');
+        return console.log('Unsuccessful');
     } else {
         freeRoll.click();
-        return console.log(false, 'Successful!');
+        return console.log('Successful!');
     }
 }
 
@@ -17,10 +33,9 @@ const cancelModal = () => {
 }
 
 const timer = document.querySelector('#time_remaining');
-console.log(timer);
-
 const freeRoll = document.getElementById('free_play_form_button');
-console.log(freeRoll);
-
 const modal = document.querySelector('.close-reveal-modal');
-console.log(modal);
+
+helper.cLog(timer);
+helper.cLog(freeRoll);
+helper.cLog(modal);
