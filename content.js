9451c3877fe8b0f16e991eba7dc45b50dd0ac685
Cuza <baopham92@yahoo.com>
2019-01-02 14:12:47 -0500

    New 24 hr test conditional added.

--- content.js
+++ content.js
@@ -18,7 +18,7 @@ const helper = Object.freeze({
         };
 
         if (localStorage.length > 0) {
-                helper.variables.counter <= localStorage.length ? helper.variables.counter + localStorage.length + 1 && logClicks() : console.log('Waiting for more rolls!');
+            helper.variables.counter <= localStorage.length ? helper.variables.counter + localStorage.length + 1 && logClicks() : console.log('Waiting for more rolls!');
         }
 
         for (var i = 0; i < localStorage.length; i++) {
@@ -58,6 +58,37 @@ const timer = document.querySelector('#time_remaining');
 const freeRoll = document.getElementById('free_play_form_button');
 const modal = document.querySelector('.close-reveal-modal');
 
+
+// Below is my experiment on finding data and logic for them. Will clean clutter and organize as a todo.
+console.log(
+    document.querySelector("#bonus_span_free_points"),
+    document.querySelector("#bonus_span_free_lott"),
+    document.querySelector("#bonus_span_fp_bonus")
+)
+
+console.log(document.querySelector('.user_reward_points').textContent)
+
+// Search for target buttons
+for (var i = 0; i <= document.getElementsByTagName('button').length; i++) {
+    
+    // Data
+    const 
+    attr = document.getElementsByTagName('button')[i],
+    rPoints = document.querySelector('.user_reward_points').textContent
+
+    if (attr.getAttribute('onclick') === 'RedeemRPProduct(\'free_points_100\')'
+        || attr.getAttribute('onclick') === 'RedeemRPProduct(\'free_lott_100\')') {
+
+        console.log(attr, rPoints)
+        (document.querySelector("#bonus_span_free_lott") === null || document.querySelector("#bonus_span_free_points" === null)) && rPoints > 2500 ? attr.click() : console.log('could not redeem rp rewards.')
+
+    } else if (attr.getAttribute('onclick') === 'RedeemRPProduct(\'fp_bonus_1000\')') {
+        
+        console.log(attr)
+        (document.querySelector("#bonus_span_fp_bonus") === null) && (rPoints > 2500 && !rPoints < 2500) ? attr.click() : console.log('could not redeem 3rd reward.')
+    }
+}
+
 helper.cLog(timer);
 helper.cLog(freeRoll);
 helper.cLog(modal);
@@ -70,17 +101,10 @@ function callback(mutationList) {
     mutationList.forEach((mutation) => {
         console.log(mutation.target.textContent, (((mutation.target.textContent - wallet) / wallet) * 100).toFixed(2))
     });
-    
-    setInterval(() => {
-        if (document.querySelector('#stop_auto_betting').style.display === 'none') {
-            document.querySelector('#start_autobet').click()
-            console.clear()
-        }
-    },
-1000)
 }
 
-var targetNode = document.querySelector('#balance') || document.querySelector('#balance2');
+var targetNode = document.querySelector('#balance') || document.querySelector('#balance2'),
+    targetNode2 = document.querySelector("#bonus_span_free_points") && document.querySelector("#bonus_span_free_lott") && document.querySelector("#bonus_span_fp_bonus")
 var observerOptions = {
     childList: true,
     attributes: true,
@@ -88,4 +112,4 @@ var observerOptions = {
 }
 
 var observer = new MutationObserver(callback);
-observer.observe(targetNode, observerOptions);
+observer.observe(targetNode2, observerOptions);
