5a65fc833fdf804270851b7f504c77f8a40e957d
Cuza <baopham92@yahoo.com>
2019-01-10 17:03:09 -0500

    Converted loop to one less performance hungry.

--- content.js
+++ content.js
@@ -25,31 +25,33 @@ const modal = document.querySelector('.close-reveal-modal')
 let buttonsRP = document.getElementsByClassName('reward_link_redeem_button_style ')
 let rpBalance = document.querySelector('.user_reward_points').textContent;
 
-for (const index of buttonsRP) {
-    const targets = [
-        'RedeemRPProduct(\'free_lott_100\')',
-        'RedeemRPProduct(\'free_points_100\')']
+const targets = [
+    'RedeemRPProduct(\'free_lott_100\')',
+    'RedeemRPProduct(\'free_points_100\')']
 
-    const match = targets.filter(t => {
-        
+targets.forEach(t => {
+    const checker = Array.from(buttonsRP).find(index => {
         if (index.getAttribute('onclick').toLocaleLowerCase().includes((t).toLocaleLowerCase())) {
             return index.click()
         }
-
-        if (!index.click()) {
-            return console.log('Rewards / Lotto - (100 Points) Claim attempt unsuccessful Or unavailable.')
-        }
     })
-}
+
+    // console.log(checker)
+    if (checker === undefined) {
+        console.log('Already have bonuses')
+    } else {
+        console.log('Claimed!')
+    }
+})
 
 // Test of auto betting with percentage tracker.
 const wallet = document.querySelector('#balance').firstChild.nodeValue || document.querySelector('#balance2').firstChild.nodeValue;
-console.log(wallet)
+// console.log(wallet)
 
 function callback(mutationList) {
     mutationList.forEach((mutation) => {
-        console.log(`Current wallet amount of: ${mutation.target.textContent}, ` (((mutation.target.textContent - wallet) / wallet) * 100).toFixed(2) `% Gained since original amount of ${wallet}.`)
-    });
+        console.log(`Current wallet amount of: ` + mutation.target.textContent, (((mutation.target.textContent - wallet) / wallet) * 100).toFixed(2) + `% Gained from ${wallet}.`)
+    })
 }
 
 var targetNode = document.querySelector('#balance') || document.querySelector('#balance2'),
