911547e242c86b4c65caf9e2721fc20d5829877c
GUL-Bao <BaoPham92@yahoo.com>
2019-10-03 15:55:28 -0400

    Version 1.2: Fixed incorrect sequence selection

--- content.js
+++ content.js
@@ -1,99 +1,120 @@
 window.onload = () => {
+
+  let rewardBtn = document.querySelector("a.rewards_link")
+  rewardBtn.click()
+
+  // const timer = document.querySelector("#time_remaining");
+  const freeRoll = document.getElementById("free_play_form_button");
+  const modal = document.querySelector(".close-reveal-modal");
+
+  // Click Free roll.
+  const clickRoll = () => {
+    if (freeRoll && freeRoll.style.display === "none") {
+      return console.log("Click for rolls currently unavailable.");
+    } else {
+      freeRoll.click();
+      return console.log("Roll clicked!");
+    }
+  };
+
+  const cancelModal = () => {
+    modal ? modal.click() : console.log("Error attempting cancel modal.");
+  };
+
+  // * INVOKE
   clickRoll();
   cancelModal();
-};
 
-// Click Free roll.
-const clickRoll = () => {
-  if (freeRoll && freeRoll.style.display === "none") {
-    return console.log("Click for rolls currently unavailable.");
-  } else {
-    freeRoll.click();
-    return console.log("Roll clicked!");
-  }
-};
+  // * ELEMENTS
+  let buttonsRP = document.getElementsByClassName("reward_link_redeem_button_style");
+  let pointsBanner = !!document.querySelector('#bonus_container_free_points');
+  let lottoBanner = !!document.querySelector('#bonus_container_free_lott');
+  let bonusBanner = !!document.querySelector('#bonus_container_fp_bonus');
+  let newArr;
 
-const cancelModal = () => {
-  modal ? modal.click() : console.log("Error attempting cancel modal.");
-};
+  // * TIMEOUT FOR RETURNING CORRECT DATA
+  window.setTimeout(() => {
+    newArr = Array.from(buttonsRP) || [...buttonsRP]
+    console.log(pointsBanner, lottoBanner)
+    console.log(newArr)
+    checkRewards()
+  }, 2000)
 
-const timer = document.querySelector("#time_remaining");
-const freeRoll = document.getElementById("free_play_form_button");
-const modal = document.querySelector(".close-reveal-modal");
+  // ? Products
+  // `RedeemRPProduct('free_points_100')` - RP Boost
+  // `RedeemRPProduct('free_lott_100')` - Tickets Boost
+  // `RedeemRPProduct('fp_bonus_1000')` - BTC Boost
+  // let rpBalance = document.querySelector(".user_reward_points").textContent;
 
-// Below is my experiment on finding data and logic for them. Will clean clutter and organize as a todo.
+  const checkRewards = () => {
+    // * TARGETS
+    const targets = [
+      `RedeemRPProduct('free_points_100')`,
+      `RedeemRPProduct('free_lott_100')`
+    ];
+    let targetResults = [];
 
-let buttonsRP = document.getElementsByClassName(
-  "reward_link_redeem_button_style "
-);
-let rpBalance = document.querySelector(".user_reward_points").textContent;
+    // * POPULATE ARRAY PLACEHOLDER WITH BUTTONS
+    targets.forEach(t => {
+      newArr.forEach(index => {
+        return index.getAttribute("onclick").toLocaleLowerCase().includes(t.toLocaleLowerCase()) === true && targetResults.push(index)
+      });
+    });
 
-// Products
-// `RedeemRPProduct('free_points_100')` - RP Boost
-// `RedeemRPProduct('free_lott_100')` - Tickets Boost
-// `RedeemRPProduct('fp_bonus_1000')` - BTC Boost
+    // ! CONFIRM DATA
+    console.log(targetResults)
 
-const targets = [`RedeemRPProduct('free_points_100')`];
+    // TODO: CREATE A DYNAMIC SOLUTION FOR CHOOSING SPECIFIC TARGET SEQUENCE
+    if (targetResults.length > 0) {
+      // * IF ARRAY IS NOT EMPTY, LOOP AND CLICK BUTTONS
+      targetResults.forEach(item => {
+        const value = item.attributes[1].value;
 
-targets.forEach(t => {
-  const checker = Array.from(buttonsRP).find(index => {
-    if (
-      index
-        .getAttribute("onclick")
-        .toLocaleLowerCase()
-        .includes(t.toLocaleLowerCase()) === true
-    ) {
-      index.removeAttribute("disabled");
-      return index.click();
-    } else if (
-      index
-        .getAttribute("onclick")
-        .toLocaleLowerCase()
-        .includes(`RedeemRPProduct('free_lott_100')`.toLocaleLowerCase()) ===
-        true &&
-      parseFloat(rpBalance.replace(/,/g, "")) > 2500
-    ) {
-      index.removeAttribute("disabled");
-      return index.click();
+        if (pointsBanner === false && value === `RedeemRPProduct('free_points_100')`) {
+          item.click()
+        }
+
+        if (lottoBanner === false && value === `RedeemRPProduct('free_lott_100')`) {
+          item.click()
+        }
+
+        // ! CURRENTLY NOT SELECTING / CLICKING BONUS REWARDS BELOW
+        // if (bonusBanner === false && value === `RedeemRPProduct('fp_bonus_1000')`) {
+        //   item.click()
+        // }
+        
+      })
     }
-  });
-
-  // console.log(checker)
-  if (checker === undefined) {
-    console.log("Already have bonuses");
-  } else {
-    console.log("Claimed!");
   }
-  return checker;
-});
 
-// Test of auto betting with percentage tracker.
-const wallet =
-  document.querySelector("#balance").firstChild.nodeValue ||
-  document.querySelector("#balance2").firstChild.nodeValue;
-// console.log(wallet)
+  // Test of auto betting with percentage tracker.
+  const wallet =
+    document.querySelector("#balance").firstChild.nodeValue ||
+    document.querySelector("#balance2").firstChild.nodeValue;
+  // console.log(wallet)
 
-function callback(mutationList) {
-  mutationList.forEach(mutation => {
-    console.log(`Current BTC: ` + mutation.target.textContent);
-    console.log(
-      (((mutation.target.textContent - wallet) / wallet) * 100).toFixed(2) +
+  function callback(mutationList) {
+    mutationList.forEach(mutation => {
+      console.log(`Current BTC: ` + mutation.target.textContent);
+      console.log(
+        (((mutation.target.textContent - wallet) / wallet) * 100).toFixed(2) +
         `% Gained from ${wallet}.`
-    );
-  });
-}
+      );
+    });
+  }
 
-var targetNode =
+  var targetNode =
     document.querySelector("#balance") || document.querySelector("#balance2"),
-  targetNode2 =
-    document.querySelector("#bonus_span_free_points") ||
-    document.querySelector("#bonus_span_free_lott") ||
-    document.querySelector("#bonus_span_fp_bonus");
-var observerOptions = {
-  childList: true,
-  attributes: true,
-  subtree: true
-};
+    targetNode2 =
+      document.querySelector("#bonus_span_free_points") ||
+      document.querySelector("#bonus_span_free_lott") ||
+      document.querySelector("#bonus_span_fp_bonus");
+  var observerOptions = {
+    childList: true,
+    attributes: true,
+    subtree: true
+  };
 
-var observer = new MutationObserver(callback);
-observer.observe(targetNode, observerOptions);
+  var observer = new MutationObserver(callback);
+  observer.observe(targetNode, observerOptions);
+};
